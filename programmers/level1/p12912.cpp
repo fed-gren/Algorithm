5e856246531b4cdead23a1ad04167ff1610c7723
fed-gren <deveb22@gmail.com>
2019-04-02 11:16:03 +0900

    두 정수 사이의 합 풀이
    반복문, swap 사용

---
+++ programmers/level1/p12912.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    int temp = 0;
+    if(a > b) {
+        temp = b;
+        b = a;
+        a = temp;
+    }
+    for(int i=a; i<=b; i+=1) {
+        answer += i;
+    }
+    cout << answer;
+    return answer;
+}
