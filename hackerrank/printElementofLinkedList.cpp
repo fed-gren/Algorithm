f66afb9031e3aae0169a2c458c3288e331562b47
fed-gren <deveb22@gmail.com>
2019-04-14 21:46:27 +0900

    Linked List 원소 출력하기 문제 풀이
    포인터 활용 링크드 리스트 순회

---
+++ hackerrank/printElementofLinkedList.cpp
@@ -0,0 +1,15 @@
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void printLinkedList(SinglyLinkedListNode* head) {
+    while(head != nullptr) {
+        cout << head->data << "\n";
+        head = head->next;
+    }
+}
