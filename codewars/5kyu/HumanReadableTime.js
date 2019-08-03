39cfdcd8b645cc196bac8c9a23d1026b812a4779
fed-gren <deveb22@gmail.com>
2019-08-03 09:49:32 +0900

    Human readable time 문제 풀이 추가

---
+++ codewars/5kyu/HumanReadableTime.js
@@ -0,0 +1,31 @@
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+
+function humanReadable(seconds) {
+  const time = {
+    h: 0,
+    m: 0,
+    s: 0
+  };
+
+  time.s = seconds;
+  time.m = parseInt(time.s / 60);
+  time.s = time.s % 60;
+
+  time.h = parseInt(time.m / 60);
+  time.m = time.m % 60;
+
+  for (let prop in time) {
+    time[prop] = time[prop].toString();
+    if (time[prop].length === 1) time[prop] = "0" + time[prop];
+  }
+
+  return `${time.h}:${time.m}:${time.s}`;
+}
+
+console.log(humanReadable(0)); // '00:00:00', 'humanReadable(0)');
+console.log(humanReadable(5)); // '00:00:05', 'humanReadable(5)');
+console.log(humanReadable(60)); // '00:01:00', 'humanReadable(60)');
+console.log(humanReadable(86399)); // '23:59:59', 'humanReadable(86399)');
+console.log(humanReadable(359999)); // '99:59:59', 'humanReadable(359999)');
