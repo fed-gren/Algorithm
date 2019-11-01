941e301c244282e3c02858a0cec08fd662b35782
fed-gren <deveb22@gmail.com>
2019-11-01 23:41:01 +0900

    Remove Duplicates from Sorted Array 문제 풀이 추가

---
+++ leetcode/Array/RemoveDuplicatesfromSortedArray.js
@@ -0,0 +1,14 @@
+/**
+ * @url https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] != nums[i])
+      nums[++i] = nums[j];
+  }
+  return ++i;
+};
